171ee300e1d56b94b7d4c4dc0ac6827320c79c8e
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-22 01:46:32 +0530

    first

---
+++ app/src/main/java/com/example/wawesoffood/splash_screen.java
@@ -0,0 +1,49 @@
+package com.example.wawesoffood;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread(){
+
+
+            private Context splash_screen;
+
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (Exception e){
+
+                }
+                finally {
+                    Intent intent = new Intent(this.splash_screen,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        };thread.start();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+}
