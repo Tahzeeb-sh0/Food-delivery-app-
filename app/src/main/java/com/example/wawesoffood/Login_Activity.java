4cdc14d0a789aa778a004876fc073169aaf86879
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-22 23:58:03 +0530

    add login page

--- app/src/main/java/com/example/wawesoffood/Login_Activity.java
+++ app/src/main/java/com/example/wawesoffood/Login_Activity.java
@@ -1,32 +1,20 @@
 package com.example.wawesoffood;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class StartActivity extends AppCompatActivity {
-AppCompatButton b1;
+public class Login_Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start);
-        b1 = findViewById(R.id.button);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(StartActivity.this,Login_Activity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
