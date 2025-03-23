bc74a6d5dd15818d92362e1b5dcd3793b381c929
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-23 17:10:52 +0530

    add signup page

--- app/src/main/java/com/example/wawesoffood/Signup_Activity.java
+++ app/src/main/java/com/example/wawesoffood/Signup_Activity.java
@@ -2,30 +2,28 @@ package com.example.wawesoffood;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login_Activity extends AppCompatActivity {
-TextView dontHaveAccount;
+public class Signup_Activity extends AppCompatActivity {
+TextView alreadyAhveAnAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        dontHaveAccount = findViewById(R.id.textView11);
-        dontHaveAccount.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_signup);
+        alreadyAhveAnAccount = findViewById(R.id.AlreadyHaveAn);
+        alreadyAhveAnAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login_Activity.this,Signup_Activity.class);
-                startActivity(intent);
+
+                Intent intent = new Intent(Signup_Activity.this,Login_Activity.class);
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
